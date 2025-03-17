327af1ff6fcb0f37a36c1c7cc976c1070fc0751d
WinGG <nguyendailong2809@gmail.com>
2025-03-18 00:23:26 +0700

    co loi

--- src/dao/NhanVienDAO.java
+++ src/dao/NhanVienDAO.java
@@ -3,91 +3,73 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dao;
+
 import java.util.*;
 import java.sql.*;
 import entity.NhanVien;
 import utils.Xjdbc;
+
 /**
  *
  * @author Admin
  */
-public class NhanVienDAO extends GymDAO<NhanVien, String>
-{       
+public class NhanVienDAO extends GymDAO<NhanVien, String> {
 
     @Override
-    public void insert(NhanVien nv) 
-    {
-       String sql="INSERT INTO NhanVien (MaNV, MatKhau, HoTen, VaiTro) VALUES (?, ?, ?, ?)";
-        Xjdbc.update(sql, 
-                nv.getMaNV(), 
-                nv.getMatKhau(), 
-                nv.getHoTen(), 
+    public void insert(NhanVien nv) {
+        String sql = "INSERT INTO NhanVien (MaNV, MatKhau, HoTen, VaiTro) VALUES (?, ?, ?, ?)";
+        Xjdbc.update(sql,
+                nv.getMaNV(),
+                nv.getMatKhau(),
+                nv.getHoTen(),
                 nv.getVaiTro());
     }
 
-    
-    
     @Override
     public void update(NhanVien nv) {
-       String sql="UPDATE NhanVien SET MatKhau=?, HoTen=?, VaiTro=? WHERE MaNV=?";
-        Xjdbc.update(sql, 
-                nv.getMatKhau(), 
-                nv.getHoTen(), 
+        String sql = "UPDATE NhanVien SET MatKhau=?, HoTen=?, VaiTro=? WHERE MaNV=?";
+        Xjdbc.update(sql,
+                nv.getMatKhau(),
+                nv.getHoTen(),
                 nv.getVaiTro(),
                 nv.getMaNV());
     }
 
     @Override
-    public void delete(String id) 
-    {
-      String sql="DELETE FROM NhanVien WHERE MaNV=?";
-        Xjdbc.update(sql,id);  
+    public void delete(String id) {
+        String sql = "DELETE FROM NhanVien WHERE MaNV=?";
+        Xjdbc.update(sql, id);
     }
-    
 
     @Override
-    public NhanVien selectById(String id) 
-    {
-     String sql="SELECT * FROM NhanVien WHERE MaNV=?";
+    public NhanVien selectById(String id) {
+        String sql = "SELECT * FROM NhanVien WHERE MaNV=?";
         List<NhanVien> list = this.selectBySql(sql, id);
-        return list.size() > 0 ? list.get(0) : null;
+        return (list != null && !list.isEmpty()) ? list.get(0) : null;
     }
-    
-       
 
     @Override
-    public List<NhanVien> selectAll()
-    {
-         String sql="SELECT * FROM NhanVien";
+    public List<NhanVien> selectAll() {
+        String sql = "SELECT * FROM NhanVien";
         return this.selectBySql(sql);
     }
 
     @Override
-    protected List<NhanVien> selectBySql(String sql, Object... args)
-    {
-      List<NhanVien> list=new ArrayList<>();
-        try {
-            ResultSet rs = null;
-            try {
-                rs = Xjdbc.query(sql, args);
-                while(rs.next()){
-                    NhanVien entity=new NhanVien();
-                    entity.setMaNV(rs.getString("MaNV"));
-                    entity.setMatKhau(rs.getString("MatKhau"));
-                    entity.setHoTen(rs.getString("HoTen"));
-                    entity.setVaiTro(rs.getBoolean("VaiTro"));
-                    list.add(entity);
-                }
-            } 
-            finally{
-                rs.getStatement().getConnection().close();
+    protected List<NhanVien> selectBySql(String sql, Object... args) {
+        List<NhanVien> list = new ArrayList<>();
+        try (ResultSet rs = Xjdbc.query(sql, args)) {
+            while (rs != null && rs.next()) {
+                NhanVien entity = new NhanVien();
+                entity.setMaNV(rs.getString(1)); // Sửa lại index bắt đầu từ 1
+                entity.setMatKhau(rs.getString(2));
+                entity.setHoTen(rs.getString(3));
+                entity.setVaiTro(rs.getBoolean(4));
+                list.add(entity);
             }
-        } 
-        catch (SQLException ex) {
+        } catch (SQLException ex) {
             ex.printStackTrace();
             throw new RuntimeException(ex);
         }
         return list;
     }
- 
 }
